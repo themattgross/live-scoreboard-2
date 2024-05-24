d4e29704dfad7aade2df9db971e107a217c0fd0c
Matt Gross <themattgross@users.noreply.github.com>
2024-05-24 17:08:02 -0500

    Setup API service, Install Bootstrap

--- src/app/mlb/mlb.component.ts
+++ src/app/mlb/mlb.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScoresService } from '../scores.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-mlb',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, NgIf, NgFor],
   templateUrl: './mlb.component.html',
-  styleUrl: './mlb.component.scss'
+  styleUrl: './mlb.component.scss',
 })
 export class MlbComponent {
+
+  constructor(private scoresService: ScoresService) { }
+
+  scoreboardData: any;
+
+  ngOnInit() {
+    this.scoresService.getMlb()
+      .subscribe(data => {
+        // Update component properties with retrieved data (data processing might be needed)
+        this.scoreboardData = data;
+        console.log(this.scoreboardData);
+      });
+  }
 
 }
---
+++ src/app/scores.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScoresService {
+
+  private scoreboardApiUrl = 'https://site.api.espn.com/apis/site/v2/sports';
+
+  constructor(private http: HttpClient) { }
+
+  getMlb(): Observable<any> {
+    const sport = "baseball";
+    const league = "mlb";
+
+    return this.getData(sport, league); // Call getData with sport and league values
+  }
+
+  getData(sport: string, league: string): Observable<any> { // Replace 'any' with a specific interface if known
+    const apiUrl = `${this.scoreboardApiUrl}/${sport}/${league}/scoreboard`;
+
+    return this.http.get<any>(apiUrl);
+  }
+}
