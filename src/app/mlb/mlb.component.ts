51c8301c3d70ff687f508b1e0cc2192b608c3198
Matt Gross <themattgross@users.noreply.github.com>
2024-05-25 15:04:55 -0500

    Data refreshes every 5 minutes using setInterval

--- src/app/mlb/mlb.component.ts
+++ src/app/mlb/mlb.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ScoresService } from '../scores.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgIf, NgFor, CommonModule } from '@angular/common';
@@ -10,19 +10,33 @@ import { NgIf, NgFor, CommonModule } from '@angular/common';
   templateUrl: './mlb.component.html',
   styleUrl: './mlb.component.scss',
 })
-export class MlbComponent {
+export class MlbComponent implements OnInit, OnDestroy {
 
   constructor(private scoresService: ScoresService) { }
 
   scoreboardData: any;
+  intervalId: any | null = null; // Initialize intervalId to null
 
   ngOnInit() {
+    this.fetchData();
+    this.intervalId = setInterval(() => this.fetchData(), 5 * 60 * 1000); // Set interval for 5 minutes (in milliseconds)
+  }
+
+  fetchData() {
     this.scoresService.getMlb()
       .subscribe(data => {
         // Update component properties with retrieved data (data processing might be needed)
         this.scoreboardData = data;
         console.log(this.scoreboardData);
       });
+      console.log("Refreshed.");
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId); // Clear the interval when the component is destroyed to prevent memory leaks
+    }
+    console.log("Destroyed.");
   }
 
 }
--- src/app/ufl/ufl.component.ts
+++ src/app/ufl/ufl.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ScoresService } from '../scores.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgIf, NgFor, CommonModule } from '@angular/common';
@@ -10,19 +10,33 @@ import { NgIf, NgFor, CommonModule } from '@angular/common';
   templateUrl: './ufl.component.html',
   styleUrl: './ufl.component.scss'
 })
-export class UflComponent {
+export class UflComponent implements OnInit, OnDestroy {
 
   constructor(private scoresService: ScoresService) { }
 
   scoreboardData: any;
+  intervalId: any | null = null; // Initialize intervalId to null
 
   ngOnInit() {
+    this.fetchData();
+    this.intervalId = setInterval(() => this.fetchData(), 5 * 60 * 1000); // Set interval for 5 minutes (in milliseconds)
+  }
+
+  fetchData() {
     this.scoresService.getUfl()
       .subscribe(data => {
         // Update component properties with retrieved data (data processing might be needed)
         this.scoreboardData = data;
         console.log(this.scoreboardData);
       });
+      console.log("Refreshed.");
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId); // Clear the interval when the component is destroyed to prevent memory leaks
+    }
+    console.log("Destroyed.");
   }
 
 }
