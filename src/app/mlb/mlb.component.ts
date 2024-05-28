bf0d05e47f987603de4aaca9bcd48522b08e772b
Matt Gross <themattgross@users.noreply.github.com>
2024-05-28 10:44:02 -0500

    Attempt browser refresh issue.

--- src/app/mlb/mlb.component.ts
+++ src/app/mlb/mlb.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ScoresService } from '../scores.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgIf, NgFor, CommonModule } from '@angular/common';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-mlb',
@@ -15,26 +16,31 @@ export class MlbComponent implements OnInit, OnDestroy {
   constructor(private scoresService: ScoresService) { }
 
   scoreboardData: any;
-  intervalId: any | null = null; // Initialize intervalId to null
+  //intervalId: any | null = null; // Initialize intervalId to null
+  dataSubscription: Subscription | null = null;
 
   ngOnInit() {
     this.fetchData();
-    this.intervalId = setInterval(() => this.fetchData(), 5 * 60 * 1000); // Set interval for 5 minutes (in milliseconds)
+    this.dataSubscription = interval(1 * 60 * 1000) // Emit every 5 minutes
+      .subscribe(() => this.fetchData());
   }
 
   fetchData() {
+
+    let date: any = new Date().toLocaleString("en-US");
+
     this.scoresService.getMlb()
       .subscribe(data => {
         // Update component properties with retrieved data (data processing might be needed)
         this.scoreboardData = data;
-        console.log(this.scoreboardData);
+        //console.log(this.scoreboardData);
       });
-      console.log("Refreshed.");
+      console.log("Refreshed " + date);
   }
 
   ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId); // Clear the interval when the component is destroyed to prevent memory leaks
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
     }
     console.log("Destroyed.");
   }
--- src/app/mls/mls.component.ts
+++ src/app/mls/mls.component.ts
@@ -18,24 +18,32 @@ export class MlsComponent implements OnInit, OnDestroy {
   intervalId: any | null = null; // Initialize intervalId to null
 
   ngOnInit() {
-    this.fetchData();
-    this.intervalId = setInterval(() => this.fetchData(), 5 * 60 * 1000); // Set interval for 5 minutes (in milliseconds)
+    console.log(this.intervalId);
+    if (!this.intervalId) { // Check if interval is already running
+      this.fetchData();
+      //this.intervalId = setInterval(() => this.fetchData(), 1 * 60 * 1000); // Set interval for 5 minutes (in milliseconds)
+      console.log(this.intervalId);
+    }
   }
 
   fetchData() {
+
+    let date: any = new Date().toLocaleString("en-US");
+
     this.scoresService.getMls()
       .subscribe(data => {
         // Update component properties with retrieved data (data processing might be needed)
         this.scoreboardData = data;
-        console.log(this.scoreboardData);
+        //console.log(this.scoreboardData);
       });
-      console.log("Refreshed.");
+      console.log("Refreshed " + date);
   }
 
   ngOnDestroy() {
     if (this.intervalId) {
       clearInterval(this.intervalId); // Clear the interval when the component is destroyed to prevent memory leaks
     }
+
     console.log("Destroyed.");
   }
 
